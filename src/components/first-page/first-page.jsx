7711187c222c9213d1ff18afe2f6c14ba3c895a7
wirapat.g <wirapat@sirisoft.co.th>
2024-10-17 16:27:25 +0700

    fix: change path of images

--- src/components/first-page/first-page.jsx
+++ src/components/first-page/first-page.jsx
@@ -3,10 +3,10 @@ import ImageStack from "../image-stack/image-stack";
 import "../image-stack/image-stack.css";
 
 const images = [
-  "public/static/images/main-img/5E15DBA2-A9AB-4D22-94F6-37BD37269DB2.JPG",
-  "public/static/images/main-img/9E20F950-208C-4446-AF5E-5FC6858EFA6E.JPG",
-  "public/static/images/main-img/IMG_3428.JPG",
-  "public/static/images/main-img/IMG_4443.jpg",
+  "/public/static/images/main-img/5E15DBA2-A9AB-4D22-94F6-37BD37269DB2.JPG",
+  "/public/static/images/main-img/9E20F950-208C-4446-AF5E-5FC6858EFA6E.JPG",
+  "/public/static/images/main-img/IMG_3428.JPG",
+  "/public/static/images/main-img/IMG_4443.jpg",
 ];
 
 function FirstPage() {
